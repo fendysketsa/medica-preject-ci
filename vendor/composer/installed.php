f4ac690fb48bd6a18421e6711bb3dc9dcebd46b4
fendysketsa <fendysketsa@gmail.com>
2021-08-22 17:15:31 +0700

    Update composer removed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '31f88e496b00107d83c2c9274d0ca3bdcf4b39a2',
+        'reference' => '5c8a6c500bb8cd5cc67426f484566a3d196fad71',
         'name' => 'codeigniter/framework',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '31f88e496b00107d83c2c9274d0ca3bdcf4b39a2',
+            'reference' => '5c8a6c500bb8cd5cc67426f484566a3d196fad71',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
